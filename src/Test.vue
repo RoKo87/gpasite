4b3fd974be01ed13e222e0ef2b45c8d02360fb49
Roko87 <rohan.konda@hotmail.com>
2022-11-14 21:14:19 -0600

    Adding gpa row details

--- src/Test.vue
+++ src/Test.vue
@@ -6,15 +6,20 @@ export default {
             name: "iphone14",
             selectedCity: null,
 		    courses: [
-                {name: 'Math', gpa: '4.0'},
+                {name: 'Math', code: '4.0'},
                 {name: 'Science', code: '3.5'},
                 {name: 'English', code: '3.0'}
-		    ]
+		    ],
+            grade: null,
+            gpa: null,
         };
     },
     watch: {
         selectedCity(){
             alert(this.selectedCity.name);
+        },
+        grade(){
+            this.gpa = this.selectedCity.code * this.grade
         }
     },
     components: {
@@ -23,9 +28,30 @@ export default {
 }
 </script>
 <template>
-    <ButtonVK title="Vamsee" />
+    <!-- <ButtonVK title="Vamsee" />
     <ButtonVK />
     <Button label = "Save" class="p-button-secondary"></Button>
-    <h1>Hello this is {{name}}</h1>
-    <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
+    <h1>Hello this is {{name}}</h1> -->
+    <div class="grid">
+        <div class="col-6">
+        <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
+        </div>
+        <div class="col-3">
+            <input v-model="grade"/>
+        </div>
+        <div class="col-3">
+            <input v-model="gpa"/>
+        </div>
+    </div>
+    <div class="grid">
+        <div class="col-6">
+        <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
+        </div>
+        <div class="col-3">
+            <input />
+        </div>
+        <div class="col-3">
+            <input />
+        </div>
+    </div>
 </template>
