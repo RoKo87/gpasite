7ff6c32ff83fe11e3b815097a9f9e9f9dd3d1603
Roko87 <rohan.konda@hotmail.com>
2022-11-22 19:54:35 -0600

    creating a component for gpa

--- src/Test.vue
+++ src/Test.vue
@@ -1,57 +1,13 @@
 <script>
-import ButtonVK from './components/Button.vue'
+import GpaRow from './components/GpaRow.vue'
 export default {
-    data() {
-        return {
-            name: "iphone14",
-            selectedCity: null,
-		    courses: [
-                {name: 'Math', code: '4.0'},
-                {name: 'Science', code: '3.5'},
-                {name: 'English', code: '3.0'}
-		    ],
-            grade: null,
-            gpa: null,
-        };
-    },
-    watch: {
-        selectedCity(){
-            alert(this.selectedCity.name);
-        },
-        grade(){
-            this.gpa = this.selectedCity.code * this.grade
-        }
-    },
     components: {
-        ButtonVK
+        GpaRow
     }
 }
 </script>
 <template>
-    <!-- <ButtonVK title="Vamsee" />
-    <ButtonVK />
-    <Button label = "Save" class="p-button-secondary"></Button>
-    <h1>Hello this is {{name}}</h1> -->
-    <div class="grid">
-        <div class="col-6">
-        <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
-        </div>
-        <div class="col-3">
-            <input v-model="grade"/>
-        </div>
-        <div class="col-3">
-            <input v-model="gpa"/>
-        </div>
-    </div>
-    <div class="grid">
-        <div class="col-6">
-        <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
-        </div>
-        <div class="col-3">
-            <input />
-        </div>
-        <div class="col-3">
-            <input />
-        </div>
-    </div>
+<div v-for="n in 5">
+    <GpaRow />
+</div>
 </template>
---
+++ src/components/GpaRow.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+    props:{
+
+    },
+    data() {
+        return {
+            name: "iphone14",
+            selectedCity: null,
+		    courses: [
+                {name: 'Math', code: '4.0'},
+                {name: 'Science', code: '3.5'},
+                {name: 'English', code: '3.0'}
+		    ],
+            grade: null,
+            gpa: null,
+        };
+    },
+    watch: {
+        selectedCity(){
+            alert(this.selectedCity.name);
+        },
+        grade(){
+            this.gpa = this.selectedCity.code * this.grade
+        }
+    },
+}
+</script>
+<template>
+    <div class="grid">
+        <div class="col-6">
+        <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
+        </div>
+        <div class="col-3">
+            <input v-model="grade"/>
+        </div>
+        <div class="col-3">
+            <input v-model="gpa"/>
+        </div>
+    </div>
+</template>
