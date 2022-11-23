cc4c7a7be8195a9af1725aa79725c6d5235e57ea
Roko87 <rohan.konda@hotmail.com>
2022-11-22 20:49:41 -0600

    adding props

--- src/Test.vue
+++ src/Test.vue
@@ -3,11 +3,31 @@ import GpaRow from './components/GpaRow.vue'
 export default {
     components: {
         GpaRow
+    },
+    data() {
+        return {
+            courses: [
+                { name: 'Math', code: '4.0' },
+                { name: 'Science', code: '3.5' },
+                { name: 'English', code: '3.0' }
+            ],
+            stats: [{
+                selectedCity: null,
+                grade: null,
+                gpa: null,
+            },
+            {
+                selectedCity: null,
+                grade: null,
+                gpa: null,
+            }
+            ]
+        }
     }
 }
 </script>
 <template>
-<div v-for="n in 5">
-    <GpaRow />
-</div>
+    <div v-for="stat in stats">
+        <GpaRow :courses=courses />
+    </div>
 </template>
--- src/components/GpaRow.vue
+++ src/components/GpaRow.vue
@@ -1,26 +1,22 @@
 <script>
-export default{
-    props:{
+import { stringifyStyle } from '@vue/shared';
 
+export default {
+    props: {
+        courses: Array
     },
     data() {
         return {
-            name: "iphone14",
             selectedCity: null,
-		    courses: [
-                {name: 'Math', code: '4.0'},
-                {name: 'Science', code: '3.5'},
-                {name: 'English', code: '3.0'}
-		    ],
             grade: null,
             gpa: null,
         };
     },
     watch: {
-        selectedCity(){
+        selectedCity() {
             alert(this.selectedCity.name);
         },
-        grade(){
+        grade() {
             this.gpa = this.selectedCity.code * this.grade
         }
     },
@@ -29,13 +25,13 @@ export default{
 <template>
     <div class="grid">
         <div class="col-6">
-        <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
+            <Dropdown v-model="selectedCity" :options="courses" optionLabel="name" placeholder="Select a Course" />
         </div>
         <div class="col-3">
-            <input v-model="grade"/>
+            <input v-model="grade" />
         </div>
         <div class="col-3">
-            <input v-model="gpa"/>
+            <input v-model="gpa" />
         </div>
     </div>
 </template>
